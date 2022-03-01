17c7044562e4cf405878dfc3d65da127f25143f0
Audrey Mengue <massoumeharmonie@gmail.com>
2022-03-01 23:30:48 +0000

    Completed sidebar and created home component

---
+++ src/components/pages/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from './home.css'
+
+function Home() {
+  return (
+    <div className='home'>Home</div>
+  )
+}
+
+export default Home
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,6 +3,11 @@ import "./sidebar.css"
 import LineStyleIcon from '@mui/icons-material/LineStyle';
 import TimelineIcon from '@mui/icons-material/Timeline';
 import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Inventory2Icon from '@mui/icons-material/Inventory2';
+import MarkEmailReadIcon from '@mui/icons-material/MarkEmailRead';
+import MessageIcon from '@mui/icons-material/Message';
+import FeedbackIcon from '@mui/icons-material/Feedback';
 
 export default function Sidebar() {
   return (
@@ -11,19 +16,59 @@ export default function Sidebar() {
           <div className="sidebarMenu">
             <h3 className="sidebarTitle">Dashboard</h3>
             <ul className="sidebarList">
-              <li className="sidebarLsitsItem">
-              <LineStyleIcon /> Home
+              <li className="sidebarLsitsItem active">
+              <LineStyleIcon className='sidebarIcon'/> Home
               </li>
               <li className="sidebarLsitsItem">
-              <TimelineIcon />Analytics
+              <TimelineIcon className='sidebarIcon'/>Analytics
               </li>
               <li className="sidebarLsitsItem">
-              <TrendingUpIcon /> Sales
+              <TrendingUpIcon className='sidebarIcon'/> Sales
               </li>
               
             </ul>
           </div>
+          
         </div>
+
+        <div className='sidebarWrapper'>
+          <div className="sidebarMenu">
+            <h3 className="sidebarTitle">Quick menu</h3>
+            <ul className="sidebarList">
+              <li className="sidebarLsitsItem active">
+              <AccountCircleIcon className='sidebarIcon'/> Users
+              </li>
+              <li className="sidebarLsitsItem">
+              <Inventory2Icon className='sidebarIcon'/>Product
+              </li>
+              <li className="sidebarLsitsItem">
+              <TrendingUpIcon className='sidebarIcon'/> Sales
+              </li>
+              
+            </ul>
+          </div>
+          
+        </div>
+
+        <div className='sidebarWrapper'>
+          <div className="sidebarMenu">
+            <h3 className="sidebarTitle">Notifications</h3>
+            <ul className="sidebarList">
+              <li className="sidebarLsitsItem active">
+              <MarkEmailReadIcon className='sidebarIcon'/> Mail
+              </li>
+              <li className="sidebarLsitsItem">
+              <MessageIcon className='sidebarIcon'/>Message
+              </li>
+              <li className="sidebarLsitsItem">
+              <FeedbackIcon className='sidebarIcon'/> Feedback
+              </li>
+              
+            </ul>
+          </div>
+          
+        </div>
+
 
     </div>
   )
