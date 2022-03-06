5cdcc48c64304500bef8a829816988fe7d7ef2e0
Audrey Mengue <massoumeharmonie@gmail.com>
2022-03-06 00:40:34 +0000

    chart colors fixed

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -82,10 +82,10 @@ export default function Chart() {
           }}
         >
           <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" stroke='#5550bd'/>
+          <XAxis dataKey="name" stroke='#203239'/>
           <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey="Active User" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="Active User" stroke="#203239" activeDot={{ r: 8 }} />
           
         </LineChart>
       </ResponsiveContainer>
